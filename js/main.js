494ac0f12a466a76ff54858253aa07d0c8c70409
qp96 <hasnaaelhassan0@gamil.com>
2021-12-06 09:49:52 +0300

    211015

---
+++ js/main.js
@@ -0,0 +1,308 @@
+$(".owl-carousel").owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 2,
+        },
+        1000: {
+            items: 3,
+        },
+    },
+});
+function blackimg() {
+    var x = document.getElementById("black");
+    var y=document.getElementById("gold");
+    var z = document.getElementById("white");
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+}
+function goldimg() {
+    var x = document.getElementById("gold");
+    var y = document.getElementById("black");
+    var z = document.getElementById("white");
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+}
+function whiteimg() {
+    var x = document.getElementById("white");
+    var y = document.getElementById("gold");
+    var z = document.getElementById("black");
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+}
+function blueimg() {
+    var x = document.getElementById("blue");
+    var y=document.getElementById("igold");
+    var z = document.getElementById("silver");
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+}
+function igoldimg() {
+    var x = document.getElementById("igold");
+    var y = document.getElementById("blue");
+    var z = document.getElementById("silver");
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+}
+function silverimg() {
+    var x = document.getElementById("silver");
+    var y = document.getElementById("igold");
+    var z = document.getElementById("blue");
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+}
+function opposilver() {
+    var x = document.getElementById("opposilver");
+    var y = document.getElementById("oppoblack");
+        x.style.display = "block";
+        y.style.display = "none";
+}
+function oppoblack() {
+    var y = document.getElementById("opposilver");
+    var x = document.getElementById("oppoblack");
+        x.style.display = "block";
+        y.style.display = "none";
+}
+function redmiblue() {
+    var x = document.getElementById("redmiblue");
+    var y = document.getElementById("redmigrey");
+        x.style.display = "block";
+        y.style.display = "none";
+}
+function redmigrey() {
+    var y = document.getElementById("redmiblue");
+    var x = document.getElementById("redmigrey");
+        x.style.display = "block";
+        y.style.display = "none";
+}
+function sumsungblack() {
+    var x = document.getElementById("sumsungblack");
+    var y = document.getElementById("sumsungwhite");
+        x.style.display = "block";
+        y.style.display = "none";
+}
+function sumsungwhite() {
+    var y = document.getElementById("sumsungblack");
+    var x = document.getElementById("sumsungwhite");
+        x.style.display = "block";
+        y.style.display = "none";
+}
+function redmi9ablue() {
+    var x = document.getElementById("redmi9a-blue");
+    var y = document.getElementById("redmi9a-green");
+    var z = document.getElementById("redmi9a-grey");
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+}
+function redmi9agreen() {
+    var x = document.getElementById("redmi9a-green");
+    var y = document.getElementById("redmi9a-blue");
+    var z = document.getElementById("redmi9a-grey");
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+}
+function redmi9agrey() {
+    var x = document.getElementById("redmi9a-grey");
+    var y = document.getElementById("redmi9a-blue");
+    var z = document.getElementById("redmi9a-green");
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+}
+function realmec25blue() {
+    var x = document.getElementById("realmec25-blue");
+    var y = document.getElementById("realmec25-grey");
+        x.style.display = "block";
+        y.style.display = "none";
+}
+function realmec25grey() {
+    var x = document.getElementById("realmec25-grey");
+    var y = document.getElementById("realmec25-blue");
+        x.style.display = "block";
+        y.style.display = "none";
+}
+function airconditionblack() {
+    var x = document.getElementById("aircondition-black");
+    var y = document.getElementById("aircondition-grey");
+    var z = document.getElementById("aircondition-red");
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+}
+function airconditiongrey() {
+    var x = document.getElementById("aircondition-grey");
+    var y = document.getElementById("aircondition-black");
+    var z = document.getElementById("aircondition-red");
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+
+}
+function airconditionred() {
+    var x = document.getElementById("aircondition-red");
+    var y = document.getElementById("aircondition-black");
+    var z = document.getElementById("aircondition-grey");
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+
+}
+function washingmachineblack() {
+    var x = document.getElementById("washingmachine-black");
+    var y = document.getElementById("washingmachine-grey");
+        x.style.display = "block";
+        y.style.display = "none";
+
+}
+function washingmachinegrey() {
+    var x = document.getElementById("washingmachine-grey");
+    var y = document.getElementById("washingmachine-black");
+        x.style.display = "block";
+        y.style.display = "none";
+}
+function huaweiblack(){
+    var x = document.getElementById("huaweiblack");
+    var y = document.getElementById("huaweigold");
+    var z = document.getElementById("huaweiwhite");
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+}
+function huaweigold(){
+    var x = document.getElementById("huaweigold");
+    var y = document.getElementById("huaweiblack");
+    var z = document.getElementById("huaweiwhite");
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+}
+function huaweiwhite(){
+    var x = document.getElementById("huaweiwhite");
+    var y = document.getElementById("huaweigold");
+    var z = document.getElementById("huaweiblack");
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+}
+function iphoneblue(){
+    var x = document.getElementById("iphoneblue");
+    var y = document.getElementById("iphonegold");
+    var z = document.getElementById("iphonesilver");
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+}
+function iphonegold(){
+    var x = document.getElementById("iphonegold");
+    var y = document.getElementById("iphoneblue");
+    var z = document.getElementById("iphonesilver");
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+}
+function iphonesilver(){
+    var x = document.getElementById("iphonesilver");
+    var y = document.getElementById("iphoneblue");
+    var z = document.getElementById("iphonegold");
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+}
+function oppo5black() {
+    var x = document.getElementById("oppo5black");
+    var y = document.getElementById("oppo5silver");
+        x.style.display = "block";
+        y.style.display = "none";
+}
+function oppo5silver() {
+    var x = document.getElementById("oppo5silver");
+    var y = document.getElementById("oppo5black");
+        x.style.display = "block";
+        y.style.display = "none";
+}
+function redmi10blue() {
+    var x = document.getElementById("redmi10blue");
+    var y = document.getElementById("redmi10grey");
+        x.style.display = "block";
+        y.style.display = "none";
+}
+function redmi10grey() {
+    var x = document.getElementById("redmi10grey");
+    var y = document.getElementById("redmi10blue");
+        x.style.display = "block";
+        y.style.display = "none";
+}
+function sumsung12black() {
+    var x = document.getElementById("sumsung12black");
+    var y = document.getElementById("sumsung12white");
+        x.style.display = "block";
+        y.style.display = "none";
+}
+function sumsung12white() {
+    var x = document.getElementById("sumsung12white");
+    var y = document.getElementById("sumsung12black");
+        x.style.display = "block";
+        y.style.display = "none";
+}
+// --------------------------------------------------------
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+}
+
+let slides = document.querySelectorAll('.slide-container');
+let index = 0;
+
+function next(){
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
+}
+
+function prev(){
+  slides[index].classList.remove('active');
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add('active');
+}
+
+document.querySelectorAll('.featured-image-1').forEach(image_1 =>{
+  image_1.addEventListener('click', () =>{
+    var src = image_1.getAttribute('src');
+    document.querySelector('.big-image-1').src = src;
+  });
+});
+
+document.querySelectorAll('.featured-image-2').forEach(image_2 =>{
+  image_2.addEventListener('click', () =>{
+    var src = image_2.getAttribute('src');
+    document.querySelector('.big-image-2').src = src;
+  });
+});
+
+document.querySelectorAll('.featured-image-3').forEach(image_3 =>{
+  image_3.addEventListener('click', () =>{
+    var src = image_3.getAttribute('src');
+    document.querySelector('.big-image-3').src = src;
+  });
+});
